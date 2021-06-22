4dcd7ea050a378cfa8d872ab3914ecb3d375469b
xoonziip <sanjivchaudhary416@gmail.com>
2021-06-22 15:17:56 +0545

    design push

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        body:['Roboto']
+      },
+      zIndex: {
+
+        '-2': '-2',
+        '-4':'-4',
+       }
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
